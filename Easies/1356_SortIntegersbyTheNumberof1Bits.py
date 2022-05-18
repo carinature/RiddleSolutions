fbb21c3f9b5adc7a0486093cb9a5334fe62c6335
karinature <karinature@protonmail.com>
2022-05-18 14:09:09 +0300

    squash! another one

--- Easies/1356_SortIntegersbyTheNumberof1Bits.py
+++ Easies/1356_SortIntegersbyTheNumberof1Bits.py
@@ -48,7 +48,6 @@ def sortByBits(arr: List[int]) -> List[int]:
     return sorted(arr, key=lambda n: (bin(n).count('1'), n))
 
 
-
 def testing():
     result = sortByBits(arr=[0, 1, 2, 3, 4, 5, 6, 7, 8])
     # print(f"result: {result}")
