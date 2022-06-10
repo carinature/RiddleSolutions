b5601190a4499874a30d37c68eb67d0e9e224df9
karinature <karinature@protonmail.com>
2022-06-10 14:26:56 +0300

    learned another one (DP - Tabulation)

---
+++ BaseAlgotirhms/DynamicProgramming/Tabulation/Fibonacci.py
@@ -0,0 +1,88 @@
+# Fibonacci in Dynamic Programing
+import collections
+import timeit
+from typing import Dict
+
+
+# Solution 1 - Iterative with Tabulation    O(n)
+def fibonacci(n: int) -> int:
+    if n < 2:
+        return n
+
+    tbl = [0] * (n + 1)
+    tbl[1] = 1
+
+    for i in range(2, n + 1):
+        tbl[i] = tbl[i - 1] + tbl[i - 2]
+
+    return tbl[n]
+
+
+def testing():
+    result = fibonacci(0)
+    assert (0 == result)
+
+    result = fibonacci(1)
+    assert (1 == result)
+
+    result = fibonacci(2)
+    assert (1 == result)
+
+    result = fibonacci(6)
+    assert (8 == result)
+
+    result = fibonacci(8)
+    assert (21 == result)
+
+    result = fibonacci(30)
+    assert (832040 == result)
+
+
+if __name__ == '__main__':
+    print("\n Finished in --- %.5f seconds ---" %
+          (timeit.timeit(testing, number=10)))
+
+#
+#
+#
+#
+#
+#
+#
+# Time complexity of the naive recursive alg would be an O(2^n):
+# if we imagine all the call to the function as a tree
+# (let's say we call the fib function with 7 as the argument)
+# then at the top of the tree we will have n=7
+# at the level below n-1=6 and n-2=5
+# and below each one 2 more nodes and the same all the way to the leave nodes
+# as a result we can visualise the calls as binary tree,
+# with depth/hight of n and therefore number of call is as the number of nodes - 2^n
+#
+# Space complexity - should count the stack of recursion calls, as well as space allocated
+# but note that not all call are made at once.
+# in fact, we only call fib(n-2) after we "closed" (poped the stack frame) of the last func call.
+# so we only allocate frames on the stack as the number of concurrent calls, which is the depth of the tree.
+# and so the space complexity is actually only O(n).
+#
+# Memoization (like Memo - a reminder / log)
+# since there are a lot of very similar calls (duplicate identical sub-trees in the calls tree)
+# (for instance we make the calls fib(3)-->[fib(1),fib(2)-->[fib(0),fib(1)]]
+#   a lot of times - for fib(3), fib(4), fib(5)...)
+# we can just "remember" the result for duplicate function calls,
+# instead of multiple calls for same value).
+# so we want to store those results that we are likely to get more than once
+# and refer to that storage, instead of make the same call again and again.
+# so time complexity goes down to O(n) -- explain!
+# since in every node n in a sub tree we have to child nodes n-1 and n-2
+# and for every node n-1 we call n-2 and n-3.
+# but for n-1 we already know the result for n-2 (because we already calculated it (for n..))
+# and the same for n-3 and so on. so instead of having a "full" binary tree we get a chain of n nodes
+# and for each of the nodes these is a sister node (except for root) - so all in all we have 2n-1 nodes.
+# each node represents a functions call (and each sister node - a hashmap/dict result
+# so the time complexity is O(n)
+# and the space complexity stays the same - O(n)
+#
+#
+#
+#
+#
--- BaseAlgotirhms/DynamicProgramming/__init__.py
+++ BaseAlgotirhms/DynamicProgramming/__init__.py
@@ -1,9 +1,27 @@
-from BaseAlgotirhms.DynamicProgramming import Fibonacci, GridTraveler
+from BaseAlgotirhms.DynamicProgramming.Memoization import Fibonacci, GridTraveler, CanSum, HowSum, BestSum
+# from BaseAlgotirhms.DynamicProgramming.Tabulation import Fibonacci, GridTraveler, CanSum, HowSum, BestSum
 
 import timeit
 
+# # # # # # # # # # # # # # # # # # # # #
+# # # #    Memoization    # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # #
+
 # Fibonacci
-print("\n Finished in --- %.5f seconds ---" % (timeit.timeit(Fibonacci.testing, number=10)))
+print("\n Finished in --- %.5f seconds ---" % (timeit.timeit(Memoization.Fibonacci.testing, number=10)))
 
 # GreedTraveler
-print("\n Finished in --- %.5f seconds ---" % (timeit.timeit(GridTraveler.testing, number=10)))
+print("\n Finished in --- %.5f seconds ---" % (timeit.timeit(Memoization.GridTraveler.testing, number=10)))
+
+# CanSum
+print("\n Finished in --- %.5f seconds ---" % (timeit.timeit(Memoization.CanSum.testing, number=10)))
+
+# HowSum
+print("\n Finished in --- %.5f seconds ---" % (timeit.timeit(Memoization.HowSum.testing, number=10)))
+
+# BestSum
+print("\n Finished in --- %.5f seconds ---" % (timeit.timeit(Memoization.BestSum.testing, number=10)))
+
+# # # # # # # # # # # # # # # # # # # # #
+# # # #     Tabulation    # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # #
